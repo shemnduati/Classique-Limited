6b7dddcfe04335364a16fdb8f3cfd948a98a2e2c
Shem Nduati <nduatishem@gmail.com>
2024-01-19 14:29:30 +0300

    final submission -update

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,7 +5,7 @@
         <h1>Reports</h1>
         <nav>
           <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
             <li class="breadcrumb-item active">Reports</li>
           </ol>
         </nav>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -5,7 +5,7 @@
         <h1>Users</h1>
         <nav>
           <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
             <li class="breadcrumb-item active">users</li>
           </ol>
         </nav>
